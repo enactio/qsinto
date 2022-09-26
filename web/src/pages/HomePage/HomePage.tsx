20f02699f766d12affa7ebee8a1d0906cca81e10
Alessandro Arpetti <arpetti@gmail.com>
2022-09-25 21:51:46 -0300

    first commit

---
+++ web/src/pages/HomePage/HomePage.tsx
@@ -0,0 +1,14 @@
+import { MetaTags } from '@redwoodjs/web'
+
+import ArticlesCells from 'src/components/ArticlesCell'
+
+const HomePage = () => {
+  return (
+    <>
+      <MetaTags title="Home" description="Home page" />
+      <ArticlesCells />
+    </>
+  )
+}
+
+export default HomePage
